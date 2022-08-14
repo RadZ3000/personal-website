f083f8c9dc994f81391d968ba89ad8dcbd9e4564
RadZ3000 <herald.adowei@gmail.com>
2022-08-14 04:02:55 -0500

    added hi.png

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,10 +5,10 @@ const Navbar = () => {
         <nav className="flex flex-row bg-gray-900 text-white py-5 font-mono">
             
             <div className="w-1/2 text-left pl-5">
-            <Link href="/"><a className="text-md font-md bg-red-700 text-black px-2 hover:text-white duration-1000"><code>&lt;herald&gt;</code></a></Link>
+            <Link href="/"><a className="text-md font-md bg-red-700 text-black px-2 hover:text-white duration-700"><code>&lt;herald&gt;</code></a></Link>
             </div>
             <div className="w-1/2 h-max text-right">
-                <Link href="/about"><a className="nav">About</a></Link>
+                <Link href="#about"><a className="nav">About</a></Link>
                 <Link href="/skills"><a className="nav">Skills</a></Link>
                 <Link href="/work"><a className="nav">Work</a></Link>
                 <Link href="/contact"><a className="nav">Contact</a></Link>
