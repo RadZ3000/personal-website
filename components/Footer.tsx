30cce403472a59c48b0ef5c2c595ca48d2b1557d
RadZ3000 <herald.adowei@gmail.com>
2022-08-18 19:58:40 -0500

    edited footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -12,7 +12,7 @@ const Footer = () => {
 
             {/* <div className="w-1/2"></div> */}
             <div className="flex flex-col justify-center gap-5 text-xs">
-                <p>Designed and Developed by <a href="#" className="socials underline">Herald Adowei</a></p>
+                <p>Designed and Developed by <a href="#" className="hover:text-red-500 hover:brightness-100 duration-500 underline">Herald Adowei</a></p>
                 <div className="flex flex-row justify-center gap-x-5">
                     <a href="#" className="footer hover:text-black"><FaGithub /></a>
                     <a href="#" className="footer hover:text-blue-700"><FaLinkedin /></a>
