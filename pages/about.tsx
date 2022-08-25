ae39f5d84a541c6c8406be7cde82dfaa9ff5d14e
RadZ3000 <herald.adowei@gmail.com>
2022-08-25 14:23:04 -0500

    index.tsx and about.tsx edit

--- pages/about.tsx
+++ pages/about.tsx
@@ -7,7 +7,7 @@ const About = () => {
   return (
     <div id="about" className="page">
 
-      <div id="intro" className="flex flex-col sm:flex-row md:flex-row lg:flex-row items-center gap-x-12 w-full lg:w-5/6 px-12 h-screen">
+      <div id="intro" className="flex flex-col sm:flex-row md:flex-row lg:flex-row items-center gap-x-12 w-full lg:w-5/6 px-12 h-full lg:h-screen">
         <div className="w-full lg:w-1/2 lg:text-justify">
           <h1 className="text-2xl md:text-2xl lg:text-3xl xl:text-3xl xl:leading-snug font-bold text-red-500 w-fit ml-12">
             <code>
