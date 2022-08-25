085d9a42d0acb7d5f7afd70e30e92c0508d6be4a
RadZ3000 <herald.adowei@gmail.com>
2022-08-25 04:54:18 -0500

    responsive contact.tsx edit

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -11,7 +11,7 @@ const Contact = () => {
 
             <div id="intro" className="flex flex-col justify-center gap-y-20 page h-screen w-screen">
 
-                <h1 className="lg:text-xl xl:text-3xl xl:leading-snug font-bold text-red-500 w-fit">
+                <h1 className="text-2xl md:text-2xl lg:text-3xl xl:text-3xl xl:leading-snug font-bold text-red-500 w-fit">
                     <code>
                         <TypeAnimation
                             sequence={[
@@ -20,27 +20,27 @@ const Contact = () => {
                         />
                     </code>
                 </h1>
-                <div className="flex flex-col gap-y-14 content-center bg-slate-800 border-slate-800 border w-2/5 rounded-xl py-8 px-16">
-                    <span className="flex flex-row items-center gap-x-20">
+                <div className="flex flex-col gap-y-14 content-center bg-slate-800 border-slate-800 border w-11/12 sm:w-2/3 md:w-3/5 lg:w-3/5 xl:w-3/5 rounded-xl py-4 px-8 lg:py-8 lg:px-16">
+                    <span className="flex flex-row gap-x-6 lg:flex-row items-center lg:gap-x-20">
                         <button className="button">
                             <FaPhone className="text-red-700" />
-                            <a href="mailto: abc@example.com">Call Me:</a>
+                            <a href="mailto: abc@example.com">Call:</a>
                         </button>
-                        <h1>+1 (123) 456–7890</h1>
+                        <h1 className="text-xs md:text-sm  lg:text-base">+1 (123) 456–7890</h1>
                     </span>
-                    <span className="flex flex-row items-center gap-x-20">
+                    <span className="flex flex-row gap-x-6 lg:flex-row items-center lg:gap-x-20">
                         <button className="button">
                             <FaEnvelope className="text-red-700" />
-                            <a href="mailto: abc@example.com">Email Me:</a>
+                            <a href="mailto: abc@example.com">Email:</a>
                         </button>
-                        <h1>first.last@email.com</h1>
+                        <h1 className="text-xs md:text-sm  lg:text-base">first.last@email.com</h1>
                     </span>
-                    <span className="flex flex-row items-center gap-x-20">
+                    <span className="flex flex-row gap-x-6 lg:flex-row items-center lg:gap-x-20">
                         <button className="button">
                             <FaComment className="text-red-700" />
-                            <a href="mailto: abc@example.com">Message Me:</a>
+                            <a href="mailto: abc@example.com">Message:</a>
                         </button>
-                        <h1>www.linkedin.com/fullname</h1>
+                        <h1 className="text-xs md:text-sm lg:text-base">linkedin.com/fullname</h1>
                     </span>
                 </div>
             </div>
