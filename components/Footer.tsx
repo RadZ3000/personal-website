27fcdc7c9d0ed407b969c8a42131eb5ca9876cc8
RadZ3000 <herald.adowei@gmail.com>
2022-08-26 15:35:51 -0500

    Footer.tsx edit

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -14,11 +14,11 @@ const Footer = () => {
             <div className="flex flex-col justify-center gap-5 text-xs">
                 <p>Designed and Developed by <a href="#" className="hover:text-red-400 hover:brightness-100 duration-500 underline">Herald Adowei</a></p>
                 <div className="flex flex-row justify-center gap-x-5">
-                    <a href="#" className="footer hover:text-black"><FaGithub /></a>
-                    <a href="#" className="footer hover:text-blue-700"><FaLinkedin /></a>
-                    <a href="#" className="footer hover:text-gray-700"><FaCodepen /></a>
-                    <a href="#" className="footer hover:text-pink-400"><FaInstagram /></a>
-                    <a href="#" className="footer hover:text-sky-400"><FaTwitter /></a>
+                    <a href="https://github.com/RadZ3000" target="_blank" rel="noopener noreferrer" className="footer hover:text-black"><FaGithub /></a>
+                    <a href="https://www.linkedin.com/heraldadowei" target="_blank" rel="noopener noreferrer" className="footer hover:text-blue-700"><FaLinkedin /></a>
+                    <a href="https://codepen.io/He_sRad" target="_blank" rel="noopener noreferrer" className="footer hover:text-gray-700"><FaCodepen /></a>
+                    <a href="https://www.instagram.com/herald_a/" target="_blank" rel="noopener noreferrer" className="footer hover:text-pink-400"><FaInstagram /></a>
+                    <a href="https://twitter.com/The_RadZee" target="_blank" rel="noopener noreferrer" className="footer hover:text-sky-400"><FaTwitter /></a>
                 </div>
             </div>
 
